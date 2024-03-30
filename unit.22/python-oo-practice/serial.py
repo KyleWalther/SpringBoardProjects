6ee18be279dac797eacfa4d6da7a220c33d57294
KyleWalther <kylerickwalther@yahoo.com>
2024-03-29 20:00:55 -0700

    updated the files

--- unit.22/python-oo-practice/serial.py
+++ unit.22/python-oo-practice/serial.py
@@ -20,14 +20,17 @@ class SerialGenerator:
     100
     """
     def __init__(self, start):
+        """make a generator, starting at start"""
         self.start = start
         self.current = start
 
     def generate(self):
+        """generate the serial number and everytime its called again, add 1 to it"""
         serialNumber = self.current
         self.current += 1
         return serialNumber
     
     def reset(self):
+        """rest the serial code back tot he original start value"""
         self.current = self.start
 
