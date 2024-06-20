1468eadab1184a6dc0451a37b688725e127f56eb
KyleWalther <kylerickwalther@yahoo.com>
2024-06-20 17:35:40 -0500

    added the second part of the exercise

--- unit.27/flask-blogly/app.py
+++ unit.27/flask-blogly/app.py
@@ -1,32 +1,29 @@
-"""Blogly application."""
-
-from flask import Flask, request, redirect, render_template, session
-
-from models import db, connect_db, User
-
-
+from flask import Flask, request, redirect, render_template, flash
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
-
-app.app_context().push()
-
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
+
+
+app.app_context().push()
+
 connect_db(app)
 db.create_all()
 
+
+
+
 @app.route('/')
 def list_users():
-    """Shows list of all users in the database"""
-    users = User.query.all()  # Retrieve all users
+    users = User.query.all()
     return render_template('home.html', users=users)
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_user():
-    """Add a new user"""
     if request.method == 'POST':
         first_name = request.form['first_name']
         last_name = request.form['last_name']
@@ -41,13 +38,11 @@ def add_user():
 
 @app.route('/user/<int:user_id>')
 def show_user(user_id):
-    """Show details of a specific user"""
-    user = User.query.get_or_404(user_id)  # Retrieve user by ID or 404 if not found
+    user = User.query.get_or_404(user_id)
     return render_template('user_detail.html', user=user)
 
 @app.route('/user/<int:user_id>/edit', methods=['GET', 'POST'])
 def edit_user(user_id):
-    """Edit a specific user's information"""
     user = User.query.get_or_404(user_id)
     if request.method == 'POST':
         user.first_name = request.form['first_name']
@@ -60,11 +55,61 @@ def edit_user(user_id):
 
 @app.route('/user/<int:user_id>/delete', methods=['POST'])
 def delete_user(user_id):
-    """Delete a specific user"""
     user = User.query.get_or_404(user_id)
     db.session.delete(user)
     db.session.commit()
     return redirect('/')
+
+@app.route("/users/<int:user_id>/post_form/new")
+def create_post(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('new_post_form.html', user=user)
+
+@app.route('/users/<int:user_id>/posts/new', methods=["POST"])
+def post_new_post(user_id):
+    user = User.query.get_or_404(user_id)
+    new_post = Post(title=request.form['title'], content=request.form['content'], user=user)
+
+    db.session.add(new_post)
+    db.session.commit()
+    return redirect(f"/user/{user_id}")
+
+@app.route('/posts/<int:post_id>')
+def show_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post_detail.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit', methods=['GET', 'POST'])
+def edit_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.content = request.form['content']
+        db.session.commit()
+
+        return redirect(f'/posts/{post_id}')
+    
+    return render_template('edit_post.html', post=post)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- unit.27/flask-blogly/models.py
+++ unit.27/flask-blogly/models.py
@@ -1,18 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
-"""Models for Blogly."""
-
 
 db = SQLAlchemy()
-# this is executing sqla alchemy
 
 def connect_db(app):
     db.app = app
     db.init_app(app)
 
-
-# models section
-
-# Models section
 class User(db.Model):
     __tablename__ = 'users'
 
@@ -21,14 +14,18 @@ class User(db.Model):
     last_name = db.Column(db.String(15), nullable=False)
     image_url = db.Column(db.String(2000), nullable=True, default='https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg')
 
+    posts = db.relationship('Post', backref='user', cascade="all, delete-orphan")
+
     def __repr__(self):
         return f'<User {self.first_name} {self.last_name}>'
     
     def greet(self):
         return f"Hi, I am {self.first_name} and my last name is {self.last_name}"
 
+class Post(db.Model):
+    __tablename__ = 'posts'
 
-
-
-
-
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(20), nullable=False)
+    content = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
